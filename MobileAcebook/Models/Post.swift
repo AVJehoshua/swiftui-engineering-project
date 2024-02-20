923155d1e5eac5546fd6db2e6e69fb21f926ff76
TomMazzag <itstomazz@gmail.com>
2024-02-20 14:18:06 +0000

    Added post fetch

---
+++ MobileAcebook/Models/Post.swift
@@ -0,0 +1,8 @@
+//
+//  Post.swift
+//  MobileAcebook
+//
+//  Created by Tom Mazzag on 20/02/2024.
+//
+
+import Foundation
---
+++ MobileAcebook/Services/PostService.swift
@@ -0,0 +1,18 @@
+//
+//  PostService.swift
+//  MobileAcebook
+//
+//  Created by Tom Mazzag on 20/02/2024.
+//
+
+import SwiftUI
+
+struct PostService: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostService()
+}
