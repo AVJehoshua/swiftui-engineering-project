c18df5ddd2ae39cf39c69e95a3e50eb3d7ee975f
Alan Gardiner <121672581+kawrou@users.noreply.github.com>
2024-02-22 14:57:57 +0000

    Wrote the service but need to update Model

--- MobileAcebook/Services/UserDataService.swift
+++ MobileAcebook/Services/UserDataService.swift
@@ -9,26 +9,30 @@ let email: String = "test@test.com"
 
 import Foundation
 
-class UserService{
-    func getuser(email: String) {
-        let url = URL(string: "http://127.0.0.1:8080/\(email)")!
+class UserService {
+    func getuser(email: String, completion: @escaping (User?) -> Void) {
+        let url = URL(string: "http://127.0.0.1:8080/users/\(email)")!
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
         
         URLSession.shared.dataTask(with: request) { data, response, error in
-            guard let data = data, error == nil else { return }
-            
+            guard let data = data, error == nil else {
+                completion(nil)
+                return
+            }
+            print(String(data: data, encoding: .utf8)!)
             do {
-                let response = try JSONDecoder().decode(PostWrapper.self, from: data)
+                let userResponse = try JSONDecoder().decode(UserResponse.self, from: data)
+                
+                let user = userResponse.user
+                print("User data received and decoded")
                 DispatchQueue.main.async {
-                    
+                    completion(user)
                 }
             } catch {
-                print("Error decoding posts: \(error)")
+                print("Error decoding user: \(error)")
+                completion(nil)
             }
         }.resume()
-        
-        return
     }
 }
-
