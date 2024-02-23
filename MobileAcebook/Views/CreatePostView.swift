e8ed7b90fced82069b884ef8379291b5123c5956
Alan Gardiner <121672581+kawrou@users.noreply.github.com>
2024-02-23 10:25:09 +0000

    To make CreatePostView functional: added createPost func from service

--- MobileAcebook/Views/CreatePostView.swift
+++ MobileAcebook/Views/CreatePostView.swift
@@ -7,58 +7,61 @@
 
 import SwiftUI
 
-func mockCreatePostService(post: String, token: String)-> Bool{
-    let result: Bool = true
-//    print(token)
-//    print(post)
-    return result
-}
+//func mockCreatePostService(post: String, token: String)-> Bool{
+//    let result: Bool = true
+////    print(token)
+////    print(post)
+//    return result
+//}
 
 
 struct CreatePostView: View {
-    @State private var newPost = ""
+    @State private var newPost: String = ""
     var token: String
+    
+    let createPostService = CreatePost()
     
     var body: some View {
         
-            HStack{
-                TextField("What's on your mind?", text: $newPost)
-                    .padding(6)
-                    .background(.gray.opacity(0.1))
-                    .cornerRadius(10)
-
-                Button("Submit"){
-                    if !newPost.isEmpty{
-                        let result = mockCreatePostService(post: newPost, token: token)
-                        
-                        if result{
+        HStack{
+            TextField("What's on your mind?", text: $newPost)
+                .padding(6)
+                .background(.gray.opacity(0.1))
+                .cornerRadius(10)
+            
+            Button("Submit"){
+                if !newPost.isEmpty{
+//                        let result = mockCreatePostService(post: newPost, token: token)
+                    createPostService.createPost(newPost: NewPost(message: newPost)) { success in
+                        if success{
                             newPost = ""
                             print("New post created")
-                        }else{
+                        }
+                        else{
                             print("Couldn't create post")
                         }
                     }
                 }
                 
-// Haven't decided which way of writing button is better depending on how we want to style it.
+                // Haven't decided which way of writing button is better depending on how we want to style it.
                 
-//                Button(action: {
-//                    if !newPost.isEmpty{
-//                        let result = mockCreatePostService(post: newPost, token: token)
-//                        if result {
-//                            newPost = ""
-//                            print("New new created")
-//                        }else{
-//                            print("Couldn't create post")
-//                        }
-//                    }
-//                }, label: {
-//                    Text("Submit")
-//                })
+                //                Button(action: {
+                //                    if !newPost.isEmpty{
+                //                        let result = mockCreatePostService(post: newPost, token: token)
+                //                        if result {
+                //                            newPost = ""
+                //                            print("New new created")
+                //                        }else{
+                //                            print("Couldn't create post")
+                //                        }
+                //                    }
+                //                }, label: {
+                //                    Text("Submit")
+                //                })
             }
+        }
     }
 }
-
 
 #Preview {
     CreatePostView(token: "Test Token")
