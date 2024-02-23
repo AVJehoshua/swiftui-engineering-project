bcde203e486b61bef49df4dc870da3091963f439
Martynas Prusevicius <mprusevicius@gmail.com>
2024-02-23 14:44:02 +0000

    Fix CreatePostView for feed update

--- MobileAcebook/Views/CreatePostView.swift
+++ MobileAcebook/Views/CreatePostView.swift
@@ -17,37 +17,35 @@ import SwiftUI
 
 struct CreatePostView: View {
     @State private var newPost: String = ""
+    @EnvironmentObject var refreshTrigger: RefreshTrigger
     
     let createPostService = CreatePost()
     
     var body: some View {
-        
-        HStack{
-            TextField("What's on your mind?", text: $newPost)
-                .padding(6)
-                .background(.white)
-                .cornerRadius(10)
-                .foregroundColor(.black)
-            
-            Button("Submit"){
-                if !newPost.isEmpty{
-                    createPostService.createPost(newPost: NewPost(message: newPost)) { result in
-                        if result{
-                            newPost = ""
-                            print("New post created")
-                        }
-                        else{
-                            print("Couldn't create post")
-                        }
+            HStack{
+                TextField("What's on your mind?", text: $newPost)
+                    .padding(6)
+                    .background(.white)
+                    .cornerRadius(10)
+                    .foregroundColor(.black)
+                
+                        Button("Submit"){
+                            if !newPost.isEmpty{
+                                createPostService.createPost(newPost: NewPost(message: newPost)) { result in
+                                    if result{
+                                        newPost = ""
+                                        print("New post created")
+                                        DispatchQueue.main.async {
+                                            refreshTrigger.trigger = true
+                                        }
+                                    }
+                                    else{
+                                        print("Couldn't create post")
+                                    }
+                                }
+                            }
+                        }.foregroundColor(.white)
                     }
                 }
-            }.foregroundColor(.white)
-        }
-    }
-}
+            }
 
-
-
-#Preview {
-    CreatePostView()
-}
